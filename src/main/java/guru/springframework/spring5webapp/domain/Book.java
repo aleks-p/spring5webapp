d05750a27ec33074cd05d0c70908748a1c554342
aleks-p <geniusap@gmail.com>
2020-07-04 13:17:34 +0200

    Fix circular dependency in toString()

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -30,6 +30,14 @@ public class Book {
         this.isbn = isbn;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getTitle() {
         return title;
     }
@@ -68,7 +76,6 @@ public class Book {
                 "id=" + id +
                 ", title='" + title + '\'' +
                 ", isbn='" + isbn + '\'' +
-                ", authors=" + authors +
                 '}';
     }
 
